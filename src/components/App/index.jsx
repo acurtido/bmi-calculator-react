535ec5a970f1fc2db4ff0a24e601ef5339945abd
Ariel Curtido <aicurtido@gmail.com>
2023-12-10 21:50:30 -0300

    initial files

---
+++ src/components/App/index.jsx
@@ -0,0 +1,12 @@
+import style from './styles.module.css'
+
+function App() {
+
+  return (
+    <div className={style.container}>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
