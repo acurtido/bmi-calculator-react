dc13f9445b82465ccec64c7abf9b91d537cbcff7
Ariel Curtido <aicurtido@gmail.com>
2023-12-11 00:41:41 -0300

    start Calculator

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,12 +1,13 @@
 import style from './styles.module.css'
 import Header from '../Header'
+import Calculator from '../Calculator'
 
 function App() {
 
   return (
     <div className={style.container}>
-      <div className={style.header}><Header/></div>
-      <div className={style.calculator}></div>
+      <Header/>
+      <Calculator/>
       <div className={style.results}></div>
       <div className={style.resultsMeaning}></div>
       <div className={style.advice}></div>
--- src/components/Calculator/index.jsx
+++ src/components/Calculator/index.jsx
@@ -2,6 +2,8 @@ import style from './styles.module.css'
 
 export default function Calculator() {
   return (
-    <div className={style.container}>BMI</div>
+    <div className={style.container}>
+      <h2>Enter your details below</h2>
+    </div>
   )
 }
